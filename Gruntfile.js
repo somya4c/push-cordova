e58209beaf0f20c9beded3c0655fa33467ee45f5
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-09-11 21:55:44 +0800

    Some code cleanup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,32 @@
-
-
 module.exports = function(grunt) {
-
     //Project configuration.
     grunt.initConfig({
         copy: {
             main: {
-                files: [
-                    {expand: true, flatten: true, src: ['node_modules/bootstrap/dist/css/bootstrap-theme.min.css'], dest: 'www/css'},
-
-                    {expand: true, flatten: true, src: ['node_modules/bootstrap/dist/css/bootstrap.min.css'], dest: 'www/css'},
-
-                    {expand: true, flatten: true, src: ['node_modules/jquery/dist/jquery.min.js'], dest: 'www/js'},
-
-                    {expand: true, flatten: true, src: ['node_modules/bootstrap/dist/js/bootstrap.min.js'], dest: 'www/js'}
-                ]
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ['node_modules/bootstrap/dist/css/bootstrap-theme.min.css'],
+                    dest: 'www/css'
+                }, {
+                    expand: true,
+                    flatten: true,
+                    src: ['node_modules/bootstrap/dist/css/bootstrap.min.css'],
+                    dest: 'www/css'
+                }, {
+                    expand: true,
+                    flatten: true,
+                    src: ['node_modules/jquery/dist/jquery.min.js'],
+                    dest: 'www/js'
+                }, {
+                    expand: true,
+                    flatten: true,
+                    src: ['node_modules/bootstrap/dist/js/bootstrap.min.js'],
+                    dest: 'www/js'
+                }]
             }
         }
-    } );
+    });
 
     // Load the plugins.
     grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -25,5 +34,4 @@ module.exports = function(grunt) {
 
     // Default task(s).
     grunt.registerTask('default', ['copy']);
-
 };
