0102142f78c590b4ed70c1bbc772a6edeae23d00
Somya <somya4c@pantarei-design.com>
2014-10-22 17:26:10 +0800

    updates for layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,77 +1,23 @@
-angular.module('starter',['starter.controllers'])
+angular.module('starter', ['starter.controllers'])
 
-.config(function($stateProvider, $routeProvider) {
+//Define Routing for app
+//  Uri /AddNewOrder -> template add_order.html and Controller AddOrderController
+//Uri /ShowOrders -> template show_orders.html and Controller AddOrderController
+sampleApp.config(['$routeProvider',
+                 function($routeProvider) {
+                     $routeProvider.
+                         when('/layout', {
+                         templateUrl: 'templates/layout.html',
+                         controller: 'layoutController'
+                     }).
+                         otherwise({
+                         redirectTo: '/layout'
+                     });
+                 }]);
 
-    //Ionic uses AngularUI Router which uses the concept of states
-    //Learn more here: https://github.com/angular-ui/ui-router
-    //  Set up the various states which the app can be in.
-    //Each state's controller can be found in controllers.js
-    $routeProvider
 
-    //setup an abstract state for the tabs directive
-    .when('tab', {
-        url: "/tab",
-        abstract: true,    
-        templateUrl: "templates/tabs.html",
-    })
+                 sampleApp.controller('layoutController', function($scope) {
 
-    //Each tab has its own nav history stack:
-    .state('tab.login', {
-        url: '/login',
-        views: {
-            'tab-login': {
-                templateUrl: 'templates/logfile/login.html',
-                controller: 'loginCtrl'
-            }
-        }
-    })
-    .state('tab.logout', {
-        url: '/logout',
-        views: {
-            'tab-logout': {
-                templateUrl: 'templates/logfile/logout.html',
-                controller: 'logoutCtrl'
-            }
-        }
-    })
-    .state('tab.register', {
-        url: '/register',
-        views: {
-            'tab-register': {
-                templateUrl: 'templates/registerfile/register.html',
-                controller: 'registerCtrl'
-            }
-        }
-    })
-    .state('tab.unregister', {
-        url: '/unregister',
-        views: {
-            'tab-unregister': {
-                templateUrl: 'templates/registerfile/unregister.html',
-                controller: 'unregisterCtrl'
-            }
-        }
-    })
-    .state('tab.channel', {
-        url: '/channel',
-        views: {
-            'tab-channel': {
-                templateUrl: 'templates/channel/channel.html',
-                controller: 'channelCtrl'
-            }
-        }
-    })
-    .state('tab.debug', {
-        url: '/debug',
-        views: {
-            'tab-debug': {
-                templateUrl: 'templates/debug/debug.html',
-                controller: 'debugCtrl'
-            }
-        }
-    });
+                     $scope.message = 'This is new a layout';
 
-    //if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/login');
-
-});
+                 });
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,19 +1,4 @@
 angular.module('starter.controllers', [])
 
-.controller('loginCtrl', function($scope) {
-})
-
-.controller('loginCtrl', function($scope) {
-})
-
-.controller('registerCtrl', function($scope) {
-})
-
-.controller('unregisterCtrl', function($scope) {
-})
-
-.controller('channelCtrl', function($scope) {
-})
-
-.controller('debugCtrl', function($scope) {
+.controller('layoutController', function($scope) {
 });
